99a9b0d6fd81a7cd2dd56122e0b062bd40ce1272
sitharaSasindu <sasindusithara@gmail.com>
2020-03-27 11:18:33 +0530

    Initial commit

---
+++ app/src/main/java/com/fyp/auth/SignUpActivity.java
@@ -0,0 +1,231 @@
+//package com.fyp.auth;
+//
+//import android.app.ProgressDialog;
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import com.fyp.kyd.R;
+//
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.text.TextUtils;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//
+//public class SignUpActivity extends AppCompatActivity {
+//
+//    private static final String TAG = "SignupActivity";
+//    private DatabaseReference mFirebaseDatabase;
+//    private FirebaseDatabase mFirebaseInstance;
+//
+//    private String userId;
+//
+//
+//    @BindView(R.id.input_name)
+//    EditText _nameText;
+//    @BindView(R.id.input_email) EditText _emailText;
+//    @BindView(R.id.input_mobile) EditText _mobileText;
+//    @BindView(R.id.input_password) EditText _passwordText;
+//    @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+//    @BindView(R.id.btn_signup)
+//    Button _signupButton;
+//    @BindView(R.id.link_login)
+//    TextView _loginLink;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sign_up);
+//        ButterKnife.bind(this);
+//
+//        _signupButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                signup();
+//            }
+//        });
+//
+//        _loginLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Finish the registration screen and return to the Login activity
+//                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+//            }
+//        });
+//
+//        mFirebaseInstance = FirebaseDatabase.getInstance();
+//
+//        // get reference to 'users' node
+//        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+//
+//        // store app title to 'app_title' node
+//        mFirebaseInstance.getReference("app_title").setValue("Realtime Database");
+//
+//        // app_title change listener
+//        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Log.e(TAG, "App title updated");
+//
+//                String appTitle = dataSnapshot.getValue(String.class);
+//
+//                // update toolbar title
+//                getSupportActionBar().setTitle(appTitle);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.e(TAG, "Failed to read app title value.", error.toException());
+//            }
+//        });
+//    }
+//
+//    private void createUser(String name, String email, String mobile) {
+//        // TODO
+//        // In real apps this userId should be fetched
+//        // by implementing firebase auth
+//        if (TextUtils.isEmpty(userId)) {
+//            userId = mFirebaseDatabase.push().getKey();
+//        }
+//
+//        UserDetails user = new UserDetails(name, email, mobile);
+//
+//        mFirebaseDatabase.child(userId).setValue(user);
+//
+//    }
+//
+//    public void signup() {
+//        Log.d(TAG, "Signup");
+//
+//        if (!validate()) {
+//            onSignupFailed();
+//            return;
+//        }
+//
+//        _signupButton.setEnabled(false);
+//
+//        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
+//                R.style.AppTheme_Dark_Dialog);
+//        progressDialog.setIndeterminate(true);
+//        progressDialog.setMessage("Creating Account...");
+//        progressDialog.show();
+//
+//        String name = _nameText.getText().toString();
+////        String address = _addressText.getText().toString();
+//        String email = _emailText.getText().toString();
+//        String mobile = _mobileText.getText().toString();
+//        String password = _passwordText.getText().toString();
+//        String reEnterPassword = _reEnterPasswordText.getText().toString();
+//
+//        // TODO: Implement your own signup logic here.
+//
+//        if (TextUtils.isEmpty(userId)) {
+//            createUser(name, email, mobile);
+//        }
+//
+//
+//
+//        new android.os.Handler().postDelayed(
+//                new Runnable() {
+//                    public void run() {
+//                        // On complete call either onSignupSuccess or onSignupFailed
+//                        // depending on success
+//                        onSignupSuccess();
+//                        // onSignupFailed();
+//                        progressDialog.dismiss();
+//                    }
+//                }, 3000);
+//    }
+//
+//
+//    public void onSignupSuccess() {
+//        _signupButton.setEnabled(true);
+//        setResult(RESULT_OK, null);
+//        finish();
+//    }
+//
+//    public void onSignupFailed() {
+//        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+//
+//        _signupButton.setEnabled(true);
+//    }
+//
+//    public boolean validate() {
+//        boolean valid = true;
+//
+//        String name = _nameText.getText().toString();
+////        String address = _addressText.getText().toString();
+//        String email = _emailText.getText().toString();
+//        String mobile = _mobileText.getText().toString();
+//        String password = _passwordText.getText().toString();
+//        String reEnterPassword = _reEnterPasswordText.getText().toString();
+//
+//        if (name.isEmpty() || name.length() < 3) {
+//            _nameText.setError("at least 3 characters");
+//            valid = false;
+//        } else {
+//            _nameText.setError(null);
+//        }
+//
+////        if (address.isEmpty()) {
+////            _addressText.setError("Enter Valid Address");
+////            valid = false;
+////        } else {
+////            _addressText.setError(null);
+////        }
+//
+//
+//        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            _emailText.setError("enter a valid email address");
+//            valid = false;
+//        } else {
+//            _emailText.setError(null);
+//        }
+//
+//        if (mobile.isEmpty() || mobile.length()!=10) {
+//            _mobileText.setError("Enter Valid Mobile Number");
+//            valid = false;
+//        } else {
+//            _mobileText.setError(null);
+//        }
+//
+//        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+//            _passwordText.setError("between 4 and 10 alphanumeric characters");
+//            valid = false;
+//        } else {
+//            _passwordText.setError(null);
+//        }
+//
+//        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+//            _reEnterPasswordText.setError("Password Do not match");
+//            valid = false;
+//        } else {
+//            _reEnterPasswordText.setError(null);
+//        }
+//
+//        return valid;
+//    }
+//
+//}
---
+++ app/src/main/java/com/fyp/kyd/MainActivity.java
@@ -0,0 +1,34 @@
+package com.fyp.kyd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//import com.fyp.auth.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button scanQR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+     scanQR.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, QRActivity.class);
+            startActivity(i);
+            finish();
+        }
+    });
+    }
+
+    private void init() {
+        scanQR = (Button) findViewById(R.id.qr);
+    }
+}
